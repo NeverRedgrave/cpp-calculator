da5605d82a1464d345f6a5c66607e3775801eccc
NeverRedgrave <ProCryonicX@yandex.ru>
2026-06-15 17:03:17 +0300

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using Number = double;
+
+bool ReadNumber(Number& number);
+void RunCalculatorCycle();
+
+#endif
