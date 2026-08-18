3a625bbfdaae1ff9be20ef2e3a53f7fc29d3145b
NeverRedgrave <ProCryonicX@yandex.ru>
2026-08-18 22:59:35 +0300

    Update mainwindow.h

--- calculator/mainwindow.h
+++ calculator/mainwindow.h
@@ -1,59 +1,39 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
+#include "enums.h"
 #include <QMainWindow>
-#include <QString>
-#include <QLabel>
-#include <QPushButton>
-#include <QGridLayout>
-#include <QVBoxLayout>
-#include "calculator.h"
+#include <functional>
+#include <string>
+#include <optional>
 
-enum class Operation {
-    NO_OPERATION,
-    MULTIPLICATION,
-    DIVISION,
-    SUBTRACTION,
-    ADDITION,
-    POWER,
-};
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() = default;
+    ~MainWindow();
 
-private slots:
-    void OnDigitPressed();
-    void OnDotPressed();
-    void OnSignPressed();
-    void OnBackspacePressed();
-    void OnEqualPressed();
-    void OnClearPressed();
-    void OnMemoryPressed();
-    void OnOperationPressed();
+    void SetInputText(const std::string& text);
+    void SetErrorText(const std::string& text);
+    void SetFormulaText(const std::string& text);
+    void SetMemText(const std::string& text);
+    void SetExtraKey(const std::optional<std::string>& key);
+
+    void SetDigitKeyCallback(std::function<void(int key)> cb);
+    void SetProcessOperationKeyCallback(std::function<void(Operation key)> cb);
+    void SetProcessControlKeyCallback(std::function<void(ControlKey key)> cb);
+    void SetControllerCallback(std::function<void(ControllerType controller)> cb);
 
 private:
-    void SetupUi();
-    void SetText(const QString& text);
-    void AddText(const QString& suffix);
-    void SetOperation(Operation op);
-    QString OpToString(Operation op);
-    QString RemoveTrailingZeroes(const QString &text);
-    QString NormalizeNumber(const QString &text);
+    Ui::MainWindow *ui;
 
-    QLabel* l_memory;
-    QLabel* l_result;
-    QLabel* l_formula;
-    QString input_number_;
-    double active_number_ = 0.0;
-    Operation current_operation_ = Operation::NO_OPERATION;
-    Calculator calculator_;
-    double memory_value_ = 0.0;
-    bool has_memory_ = false;
-    bool is_result_shown_ = false; 
+    std::function<void(int key)> digit_cb_;
+    std::function<void(Operation key)> operation_cb_;
+    std::function<void(ControlKey key)> control_cb_;
+    std::function<void(ControllerType controller)> controller_cb_;
 };
-
-#endif
